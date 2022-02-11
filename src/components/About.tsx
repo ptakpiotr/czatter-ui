8c46ef0fcc068ee2e855e6939af628beb3aaea8c
ptakpiotr <piter020@outlook.com>
2022-02-11 14:09:37 +0100

    Finished About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,10 +6,11 @@ function About() {
       Simple chatting app created with React and TypeScript. <br /> More details
       about this project:
       <ul>
-        <li>Frontend in React & Typescript</li>
+        <li>Frontend in React {"&"} Typescript</li>
         <li>Backend in .NET 6 WebApi</li>
         <li>Layout created with the help of bootstrap</li>
         <li>Messaging done with the usage of SignalR</li>
+        <li>Icons from react-icons package, images from unsplash.com</li>
       </ul>
       More projects on my{" "}
       <a href="https://github.com/ptakpiotr" target="_blank" rel="noreferrer">
